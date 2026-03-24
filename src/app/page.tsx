4883edfc013f7bcab4e30c647e93a5320dd441ec
Varad163 <varadgholap163@gmail.com>
2026-03-24 11:07:43 +0530

    foundation folders

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold">
+        Golf Charity Platform ⛳
+      </h1>
+    </main>
+  )
+}
